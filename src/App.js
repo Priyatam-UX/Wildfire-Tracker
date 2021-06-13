565e901d2968d1bbc44cb64e83a5193326f27297
Priyatam Chinnari <33813704+Priyatam-UX@users.noreply.github.com>
2021-06-13 14:02:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   useEffect(() => {
     const fetchEvents = async () => {
       setLoading(true)
-      const res = await fetch('https://eonet.sci.gsfc.nasa.gov/api/v2.1/events')
+      const res = await fetch('https://eonet.sci.gsfc.nasa.gov/api/v2.1/events');
       const { events } = await res.json()
 
       setEventData(events)
